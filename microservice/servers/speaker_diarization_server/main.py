69ac8515b814690c3f7524c215f0793e6607b625
XueKX <xuekaixiang@lyxxkj.com.cn>
2023-08-09 13:24:34 +0800

    code format

--- microservice/servers/speaker_diarization_server/main.py
+++ microservice/servers/speaker_diarization_server/main.py
@@ -19,8 +19,7 @@ from tqdm.contrib.concurrent import process_map
 from loguru import logger
 
 name = os.path.basename(__file__).split(".")[0]
-logger.add("log/"+name+"_{time}.log", rotation="500 MB", encoding="utf-8",
-           enqueue=True, compression="zip", backtrace=True, diagnose=True)
+logger.add("log/"+name+"_{time}.log", rotation="500 MB", encoding="utf-8", enqueue=True, compression="zip", backtrace=True, diagnose=True)
 
 host = "http://192.168.3.169"
 vad_url = f"{host}:5005/energy_vad/file"  # VAD
@@ -28,14 +27,14 @@ lang_url = f"{host}:5002/lang_classify"  # 语种识别
 encode_url = f"{host}:5001/encode"  # 提取特征
 cluster_url = f"{host}:5011/cluster"  # cluster
 asr_url = f"{host}:5000/transcribe/file"  # ASR
-use_model_type = "ECAPATDNN"
+
+use_model_type = "ERES2NET_Base"
 msg_db = cfg.MYSQL
 
 
 def send_request(url, method='POST', files=None, data=None, json=None, headers=None):
     try:
-        response = requests.request(
-            method, url, files=files, data=data, json=json, headers=headers)
+        response = requests.request(method, url, files=files, data=data, json=json, headers=headers)
         response.raise_for_status()
         return response.json()
     except requests.exceptions.RequestException as e:
@@ -57,8 +56,7 @@ def find_items_with_highest_value(dictionary):
     for key, value in dictionary.items():
         if value_counts[value] == max_count:
             keys_with_max_value = value
-    items_with_highest_value = {key: value for key, value in dictionary.items(
-    ) if value_counts[value] == max_count}
+    items_with_highest_value = {key: value for key, value in dictionary.items() if value_counts[value] == max_count}
     return items_with_highest_value, keys_with_max_value
 
 
@@ -75,8 +73,7 @@ def pipeline(tmp_folder, filepath, spkid):
     d = {}
     for idx, i in enumerate(response['timelist']):
         output_file = f"{tmp_folder}/{spkid}_{idx}.wav"  # 截取后的音频片段保存路径
-        extract_audio_segment(filepath, output_file,
-                              start_time=i[0]/1000, end_time=i[1]/1000)
+        extract_audio_segment(filepath, output_file, start_time=i[0]/1000, end_time=i[1]/1000)
         output_file_li.append(output_file)
         d[output_file] = (i[0]/1000, i[1]/1000)
 
@@ -87,11 +84,9 @@ def pipeline(tmp_folder, filepath, spkid):
     if response['code'] == 200:
         pass_list = response['pass_list']
         url_list = response['file_url_list']
-        mandarin_wavs = [
-            i for i in url_list if pass_list[url_list.index(i)] == 1]
+        mandarin_wavs = [i for i in url_list if pass_list[url_list.index(i)] == 1]
     else:
-        logger.error(
-            f"Lang_classify failed. spkid:{spkid}.response:{response}")
+        logger.error(f"Lang_classify failed. spkid:{spkid}.response:{response}")
         return None
 
     # step4 提取特征
@@ -114,31 +109,24 @@ def pipeline(tmp_folder, filepath, spkid):
     }
     response = send_request(cluster_url, json=data)
     logger.info(f"\t * -> Cluster result: {response}")
-    items, keys_with_max_value = find_items_with_highest_value(
-        response['labels'])
+    items, keys_with_max_value = find_items_with_highest_value(response['labels'])
     max_score = response['scores'][keys_with_max_value]['max']
     min_score = response['scores'][keys_with_max_value]['min']
 
     if min_score < 0.8:
-        logger.info(
-            f"After cluster min_score  < 0.8. spkid:{spkid}.response:{response['scores']}")
+        logger.info(f"After cluster min_score  < 0.8. spkid:{spkid}.response:{response['scores']}")
         return None
     total_duration = 0
     for i in items.keys():
         total_duration += file_emb['length'][i]
     if total_duration < cfg.MIN_LENGTH_REGISTER:
-        logger.info(
-            f"After cluster total_duration:{total_duration} < {cfg.MIN_LENGTH_REGISTER}s. spkid:{spkid}.response:{response}")
+        logger.info(f"After cluster total_duration:{total_duration} < {cfg.MIN_LENGTH_REGISTER}s. spkid:{spkid}.response:{response}")
         return None
-    selected_files = sorted(items.keys(), key=lambda x: x.split(
-        "/")[-1].replace(".wav", "").split("_")[0])
-    audio_data = np.concatenate([torchaudio.load(file.replace(
-        "local://", ""))[0] for file in selected_files], axis=-1)
-    torchaudio.save(os.path.join(tmp_folder, f"{spkid}_selected.wav"), torch.from_numpy(
-        audio_data), sample_rate=8000)
+    selected_files = sorted(items.keys(), key=lambda x: x.split("/")[-1].replace(".wav", "").split("_")[0])
+    audio_data = np.concatenate([torchaudio.load(file.replace("local://", ""))[0] for file in selected_files], axis=-1)
+    torchaudio.save(os.path.join(tmp_folder, f"{spkid}_selected.wav"), torch.from_numpy(audio_data), sample_rate=8000)
 
-    selected_times = [d[_data.replace("local://", "")]
-                      for _data in selected_files]
+    selected_times = [d[_data.replace("local://", "")] for _data in selected_files]
 
     # step6 ASR
     text = ""
@@ -147,10 +135,8 @@ def pipeline(tmp_folder, filepath, spkid):
     response = send_request(asr_url, files=files, data=data)
     if response.get('transcription') and response.get('transcription').get('text'):
         text = response['transcription']["text"]
-        # logger.info(f"\t * -> ASR结果: {text}")
     else:
-        logger.error(
-            f"ASR failed. spkid:{spkid}.message:{response['message']}")
+        logger.error(f"ASR failed. spkid:{spkid}.message:{response['message']}")
 
     # step7 NLP
     # nlp_result = classify_text(text)
@@ -164,8 +150,7 @@ def pipeline(tmp_folder, filepath, spkid):
 
     # step8 上传OSS
     raw_url = upload_file("raw", filepath, f"{spkid}/raw_{spkid}.wav")
-    selected_url = upload_file("raw", os.path.join(
-        tmp_folder, f"{spkid}_selected.wav"), f"{spkid}/{spkid}_selected.wav")
+    selected_url = upload_file("raw", os.path.join(tmp_folder, f"{spkid}_selected.wav"), f"{spkid}/{spkid}_selected.wav")
 
     return {
         "spkid": spkid,
@@ -190,8 +175,7 @@ def insert_to_db(data):
     cursor = conn.cursor()
     try:
         sql = "INSERT INTO check_for_speaker_diraization (`record_id`, `file_url`, `selected_url`, `asr_text`, `wav_duration`,`create_time`,`selected_times`) VALUES (%s, %s, %s, %s, %s,now(), %s);"
-        cursor.execute(sql, (data['spkid'], data['raw_file_path'],
-                       data['selected_url'], data['asr_result'], data['total_duration'], str(data['selected_times'])))
+        cursor.execute(sql, (data['spkid'], data['raw_file_path'], data['selected_url'], data['asr_result'], data['total_duration'], str(data['selected_times'])))
         conn.commit()
     except Exception as e:
         logger.error(f"Insert to db failed. spkid:{data['spkid']}. msg:{e}.")
