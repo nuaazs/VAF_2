2cc258fe077b07b7b0a137d2bb7ffd42c7af1ab1
nuaazs <zhaosheng@nuaa.edu.cn>
2023-11-01 08:48:51 +0800

    ADD build.sh

---
+++ cpp/top_src/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Build top1_multi_model
+echo "Build ../bin/top1_multi_model"
+echo "    *-> Used for Multi Model"
+g++ get_top1_and_score.cpp -std=c++11 -mfma -O3 -Ofast -ffast-math -fopenmp -lopenblas -o ../bin/top1_multi_model
+
+# Build top1acc
+echo "Build ../bin/top1acc"
+echo "    *-> Used for Calc ACC result from Multi Model"
+g++ get_top1_acc.cpp -std=c++17 -mfma -O3 -Ofast -ffast-math -fopenmp -lopenblas -o ../bin/top1acc
+
+# Build eer
+echo "Build ../bin/eer"
+echo "    *-> Used for Calc EER result from Multi Model"
+g++ get_eer.cpp -std=c++11 -mfma -O3 -Ofast -ffast-math -fopenmp -lopenblas -o ../bin/eer
